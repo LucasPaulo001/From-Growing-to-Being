88bf234127c185001067e829721d764f5b4ba7a1
Lucas Paulo <lp152659@gmail.com>
2024-10-16 20:58:51 -0300

    Adição de animação nos questionários

---
+++ js/reveal.js
@@ -0,0 +1,16 @@
+window.efect = ScrollReveal({reset:true})
+
+efect.reveal('.efect-form', 
+{
+    duration: 1000,
+    distance: '90px',
+    delay: '100',
+    origin: 'right'
+})
+efect.reveal('.efect-form02', 
+{
+    duration: 1000,
+    distance: '90px',
+    delay: '100',
+    origin: 'left'
+})
